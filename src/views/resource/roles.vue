95ae34626f3af1907b1278379992dce39ff3a47c
施亚萍 <admine3xample@qq.com>
2021-03-21 17:55:50 +0800

    更新权限管理

--- src/views/resource/roles.vue
+++ src/views/resource/roles.vue
@@ -9,10 +9,9 @@
         label-position="left"
         class="news-form"
       >
-        <el-form-item label="用户" prop="name">
-          <el-select v-model="form.name" placeholder="请选择下属账户">
-            <el-option label="区域一" value="shanghai" />
-            <el-option label="区域二" value="beijing" />
+        <el-form-item label="用户" prop="userId">
+          <el-select v-model="form.userId" placeholder="请选择下属账户" @change="setRoles">
+            <el-option v-for="item in options" :key="item.id" :label="item.username" :value="item.id" />
           </el-select>
         </el-form-item>
         <el-form-item label="访问控制" prop="name" placeholder="请输入杂草来源">
@@ -62,28 +61,85 @@
 </template>
 
 <script>
+import { getUserPage, setPrivGroup } from '@/api/admin'
+import { mapGetters } from 'vuex'
 
 export default {
 
   data() {
     return {
       form: {
-        name: '',
+        userId: '',
         desc: '',
-        region1: '',
-        region2: '',
-        region3: '',
         from: ''
       },
-      checkList: []
+      options: [],
+      checkList: [],
+      privGroup: [
+        { pageName: 'WeedsManagement', visible: 0, label: '杂草管理' },
+        { pageName: 'Search', visible: 0, label: '杂草检索' },
+        { pageName: 'Information', visible: 0, label: '资料管理' },
+        { pageName: 'InformationSearch', visible: 0, label: '资料检索' },
+        { pageName: 'ExpertManagement', visible: 0, label: '专家管理' },
+        { pageName: 'Management', visible: 0, label: '新闻管理' },
+        { pageName: 'Log', visible: 0, label: '日志管理' },
+        { pageName: 'Account', visible: 0, label: '账户管理' },
+        { pageName: 'SyncAccount', visible: 0, label: '账户同步' },
+        { pageName: 'File', visible: 0, label: '文件管理' },
+        { pageName: 'Space', visible: 0, label: '空间管理' }
+      ]
     }
+  },
+  computed: {
+    ...mapGetters([
+      'userId'
+    ])
   },
   mounted() {
     console.log(this.$route.params, 'sss')
+    this.queryUserList()
   },
   methods: {
+    queryUserList() {
+      getUserPage().then((res) => {
+        const { data } = res
+        this.options = data.userlist
+      })
+    },
+    setRoles() {
+      const user = this.options.find(item => item.id === this.form.userId)
+      const { privGroup } = user
+      if (privGroup && privGroup.length === 1 && privGroup[0].pageName === '*' && privGroup[0].visible === 1) {
+        // 设置权限
+        const arr = []
+        this.privGroup.forEach(item => {
+          arr.push(item.label)
+        })
+        this.checkList = arr
+      }
+    },
     onSubmit() {
-      console.log('submit!')
+      const arr = []
+      this.privGroup.forEach(item => {
+        if (this.checkList.includes(item.label)) {
+          arr.push({
+            pageName: item.pageName,
+            visible: 1
+          })
+        }
+      })
+      const params = {
+        userId: this.userId,
+        privGroup: JSON.stringify(arr)
+      }
+      setPrivGroup(params).then(data => {
+        if (data.state === 1) {
+          this.$message({
+            type: 'success',
+            message: '设置成功!'
+          })
+        }
+      })
     }
   }
 }
